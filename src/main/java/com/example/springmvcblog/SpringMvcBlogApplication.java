658303bddcc6ce523c145f9136ac1c0878105b38
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2020-01-25 11:50:37 +0630

    start online shop

---
+++ src/main/java/com/example/springmvcblog/SpringMvcBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.springmvcblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springmvcblog/SpringMvcBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmvcblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
